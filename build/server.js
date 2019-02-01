2bf5cd90095b9b061fdd825c454fa1af666fd945
ekikoh <faityfaity1@gmail.com>
2019-02-01 14:28:47 +0100

    Prepare for build

--- build/server.js
+++ build/server.js
@@ -19,6 +19,7 @@ var _officeRoute2 = _interopRequireDefault(_officeRoute);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
+var port = process.env.PORT || 3000;
 
 app.use(_express2.default.json());
 
@@ -26,8 +27,8 @@ app.get('/', function (req, res) {
   return res.status(200).send({ message: 'Welcome to Politico API' });
 });
 
-app.listen(3000);
-console.log('app running on port ', 3000);
+app.listen(port);
+console.log('app running on port ', port);
 
 (0, _partyRoute2.default)(app);
 (0, _officeRoute2.default)(app);
