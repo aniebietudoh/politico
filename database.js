e590d515ab6d55b2175247a818d7834d985c55cf
ekikoh <faityfaity1@gmail.com>
2019-02-05 14:58:26 +0100

    [feature #163726927] create political party

---
+++ database.js
@@ -0,0 +1,58 @@
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+});
+
+pool.on('connect', () => {
+  console.log('connected to the db');
+});
+
+const createTables = () => {
+  const queryText =
+    `CREATE TABLE IF NOT EXISTS
+      parties(
+        id UUID PRIMARY KEY,
+        party_name VARCHAR(128) NOT NULL,
+        party_address VARCHAR(128) NOT NULL,
+        party_logo VARCHAR(128) NOT NULL
+      )`;
+
+  pool.query(queryText)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
+}
+
+const dropTables = () => {
+  const queryText = 'DROP TABLE IF EXISTS parties';
+  pool.query(queryText)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
+}
+
+pool.on('remove', () => {
+  console.log('client removed');
+  process.exit(0);
+});
+
+module.exports = {
+  createTables,
+  dropTables
+};
+
+require('make-runnable');
--- server/server.js
+++ server/server.js
@@ -1,16 +1,19 @@
 import express from 'express';
-import Party from './src/controllers/partyController';
-import Office from './src/controllers/officeController';
+import dotenv from 'dotenv';
+import 'babel-polyfill';
+import objectsAPI from './src/objectsAPI/controllers/officeController';
+import databaseAPI from './src/databaseAPI/controllers/partyController';
 
-const app = express();
+dotenv.config();
+const Party = process.env.TYPE === 'database' ? databaseAPI : objectsAPI;
+const app = express()
 
-app.use(express.json());
-
+app.use(express.json())
 app.get('/', (req, res) => res.status(200).send({ message: 'Welcome to politico API' }));
 
 
 app.post('/api/v1/parties', Party.createParty);
-app.get('/api/v1/parties', Party.getAllParty);
+/*app.get('/api/v1/parties', Party.getAllParty);
 app.get('/api/v1/parties/:id', Party.getOneParty);
 app.patch('/api/v1/parties/:id', Party.updateParty);
 app.delete('/api/v1/parties/:id', Party.deleteParty);
@@ -18,7 +21,7 @@ app.delete('/api/v1/parties/:id', Party.deleteParty);
 app.post('/api/v1/offices', Office.createOffice);
 app.get('/api/v1/offices', Office.getAllOffice);
 app.get('/api/v1/offices/:id', Office.getOneOffice);
-
+*/
 
 app.listen(3000);
 console.log('app running on port ', 3000);
