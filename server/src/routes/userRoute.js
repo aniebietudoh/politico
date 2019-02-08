e4233b656869f22f5cca310352899c6b506f1c28
ekikoh <faityfaity1@gmail.com>
2019-02-08 09:45:58 +0100

    change route name

--- server/src/routes/userRoute.js
+++ server/src/routes/userRoute.js
@@ -2,6 +2,6 @@ import Auth from '../middleware/Auth';
 import dbUser from '../controllers/userController';
 
 export default app => {
-	app.post('/api/v1/users', dbUser.create);
-	app.post('/api/v1/users/login', dbUser.login);
+	app.post('/auth/signup', dbUser.create);
+	app.post('/auth/login', dbUser.login);
 };
